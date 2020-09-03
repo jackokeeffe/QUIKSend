d0f29e18580fd6fd8c12d448bc741476d9737c05
Jack <jokeeffe@rsgc.on.ca>
2020-09-03 19:30:14 -0400

    Initial Commit

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     url='https://github.com/jackokeeffe/QUIKSend',
     license='',
-    author='Jack',
+    author='Jack OKeeffe',
     author_email='okeeffe.jpo@protonmail.com',
     description='Send emails from a GUI created in Python',
     install_requires=['PySimpleGui'],
